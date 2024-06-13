0d506c0d4672d48470859d6922f381fc9c7e9aff
Samson Okeji <engrsamsonokeji@gmail.com>
2024-06-13 05:34:41 +0100

    updated terra

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -8,14 +8,6 @@ variable "region" {
 }
 
 
-module "ec2" {
-  source = "../../modules/ec2"
-  subnet_id = var.subnet_id
-  cluster_name = var.cluster_name
-  subnets = var.subnets
-  vpc_id = var.vpc_id
-
-}
 
 module "ecr" {
   source = "../../modules/ecr"
